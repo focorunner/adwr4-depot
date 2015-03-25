6a74bccac57f7cb7bee45d7796c355d5d24dc4d2
Mark Coleman <mark@focorunner.com>
2015-03-25 17:05:38 -0600

    Improve Cart.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,16 +1,5 @@
 class StoreController < ApplicationController
-  before_action :increment_product_index_counter
-
   def index
     @products = Product.order(:title)
   end
-
-  private
-    def increment_product_index_counter
-      if session[:counter].nil?
-        session[:counter] = 1
-      else
-        session[:counter] += 1
-      end
-    end
 end
