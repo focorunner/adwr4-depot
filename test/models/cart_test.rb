caf93a945ab141035b25060d4c5b5a0ce5f87c54
Mark Coleman <mark@focorunner.com>
2015-03-25 20:12:52 -0600

    Refactor add product to cart unit tests.

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,23 +1,24 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
+  def setup
+    @cart = Cart.create
+    @product_one = products(:one)
+    @product_two = products(:two)
+  end
+
   test "add unique products" do
-    cart = Cart.create
-    product_one = products(:one)
-    product_two = products(:two)
-    cart.add_product(product_one.id).save!
-    cart.add_product(product_two.id).save!
-    assert_equal 2, cart.line_items.size
-    assert_equal product_one.price + product_two.price, cart.total_price
+    @cart.add_product(@product_one.id).save!
+    @cart.add_product(@product_two.id).save!
+    assert_equal 2, @cart.line_items.size, "line item # incorrect"
+    assert_equal @product_one.price + @product_two.price, @cart.total_price, "total price icorrect"
   end  
 
   test "add duplicate products" do
-    cart = Cart.create
-    product = products(:ruby)
-    cart.add_product(product.id).save!
-    cart.add_product(product.id).save!
-    assert_equal 2 * product.price, cart.total_price, "total price icorrect"
-    assert_equal 1, cart.line_items.size, "line item # incorrect"
-    assert_equal 2, cart.line_items.sum(:quantity), "final quantity is incorrect"
+    @cart.add_product(@product_one.id).save!
+    @cart.add_product(@product_one.id).save!
+    assert_equal 2 * @product_one.price, @cart.total_price
+    assert_equal 1, @cart.line_items.size
+    assert_equal 2, @cart.line_items.sum(:quantity), "final quantity is incorrect"
   end
 end
