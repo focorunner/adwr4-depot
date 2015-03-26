f67639be0bd7b01dc70e90bc48507550dc8484e7
Mark Coleman <mark@focorunner.com>
2015-03-25 18:24:50 -0600

    Add store index page visit counter.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
   def index
+    @counter = increment_counter
     @products = Product.order(:title)
   end
+
+  private
+    def increment_counter
+      if session[:counter].nil?
+        session[:counter] = 1
+      else
+        session[:counter] += 1
+      end
+    end
 end
