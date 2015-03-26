e417794798241db884450bdba7adac6d5d1c744a
Mark Coleman <mark@focorunner.com>
2015-03-25 20:08:05 -0600

    Add unit tests for adding products to cart.

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add unique products" do
+    cart = Cart.create
+    product_one = products(:one)
+    product_two = products(:two)
+    cart.add_product(product_one.id).save!
+    cart.add_product(product_two.id).save!
+    assert_equal 2, cart.line_items.size
+    assert_equal product_one.price + product_two.price, cart.total_price
+  end  
+
+  test "add duplicate products" do
+    cart = Cart.create
+    product = products(:ruby)
+    cart.add_product(product.id).save!
+    cart.add_product(product.id).save!
+    assert_equal 2 * product.price, cart.total_price, "total price icorrect"
+    assert_equal 1, cart.line_items.size, "line item # incorrect"
+    assert_equal 2, cart.line_items.sum(:quantity), "final quantity is incorrect"
+  end
 end
